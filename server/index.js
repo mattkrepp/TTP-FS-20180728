b8aefa6136cc88e08a636ddec3b13f9269d14ae0
Matt Krepp <mattkrepp@gmail.com>
2018-12-01 17:12:30 -0500

    Core app architecture

---
+++ server/index.js
@@ -0,0 +1,101 @@
+const path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const session = require('express-session');
+const passport = require('passport');
+const SequelizeStore = require('connect-session-sequelize')(session);
+const db = require('./db');
+const sessionStore = new SequelizeStore({db});
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+module.exports = app;
+
+//Ensures Mocha quits after tests complete.
+if (process.env.NODE_ENV === 'test') {
+  after('close the session store', () => sessionStore.stopExpiringSessions())
+}
+
+if (process.env.NODE_ENV !== 'production') require('../secrets');
+
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await db.models.user.findById(id)
+    done(null, user)
+  } catch (err) {
+    done(err)
+  }
+})
+
+const createApp = () => {
+  // logging middleware
+  app.use(morgan('dev'))
+
+  // body parsing middleware
+  app.use(express.json())
+  app.use(express.urlencoded({extended: true}))
+
+  // compression middleware
+  app.use(compression())
+
+  // session middleware with passport
+  app.use(
+    session({
+      secret: process.env.SESSION_SECRET || 'my best friend is Cody',
+      store: sessionStore,
+      resave: false,
+      saveUninitialized: false
+    })
+  )
+  app.use(passport.initialize())
+  app.use(passport.session())
+
+  // auth and api routes
+  app.use('/auth', require('./auth'))
+  app.use('/api', require('./api'))
+
+  // static file-serving middleware
+  app.use(express.static(path.join(__dirname, '..', 'public')))
+
+  // any remaining requests with an extension (.js, .css, etc.) send 404
+  app.use((req, res, next) => {
+    if (path.extname(req.path).length) {
+      const err = new Error('Not found')
+      err.status = 404
+      next(err)
+    } else {
+      next()
+    }
+  })
+
+  // sends index.html
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'))
+  })
+
+  // error handling endware
+  app.use((err, req, res, next) => {
+    console.error(err)
+    console.error(err.stack)
+    res.status(err.status || 500).send(err.message || 'Internal server error.')
+  })
+}
+
+const startListening = () => {
+  const server = app.listen(PORT, () =>
+    console.log(`Server listening on port ${PORT}`)  
+  )
+}
+
+
+async function bootApp() {
+  await sessionStore.sync();
+  await db.sync();
+  await createApp();
+  await startListening();
+}
+
+bootApp();
